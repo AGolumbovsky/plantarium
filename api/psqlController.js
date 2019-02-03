710ccafd6e1dd1fc638738ef9bffe41494b4ceff
agolumbovsky <agolumbovsky@gmail.com>
2019-02-02 19:30:38 -0500

    conncected to postgres and able to query

--- api/psqlController.js
+++ api/psqlController.js
@@ -1,7 +1,31 @@
-const pg = require('pg');
+const { Pool, Client } = require('pg');
 
-const pool = new pg.Pool({
-    user: 'postgre',
-    host: '127.0.0.1',
-    database: ''
+var connectionString = "postgres://plantr:ko00KO))@localhost/plantarium_db";
+
+const pool = new Pool({
+    connectionString: connectionString
+});
+
+pool.query(`INSERT INTO readings (reading, identifier)
+            VALUES (343, 'take this')`, (err, res) => {
+    if (err) {
+        throw err;
+    }
+
+    console.log("INSERT query executed", res);
+    
+              
+});
+
+pool.query('SELECT * from readings order by id desc', (err, res) => {
+
+    if (err) {
+        throw err;
+    }
+
+    console.log(res.rows[0]);
+    pool.end();
+  
 })
+// await pool.end();
+
